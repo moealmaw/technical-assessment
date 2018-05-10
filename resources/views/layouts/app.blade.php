2db91881279dc797f2c2610f36226b324833a07c
Mohammad Almawali <moealmaw@gmail.com>
2018-05-10 05:56:42 +0300

    app master layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,23 @@
-<html>
+<!doctype html>
+<html lang="{{ app()->getLocale() }}" class="bg-white antialiased h-full relative">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+    <link type="text/css" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500">
+
+</head>
+<body class="font-source-sans font-normal text-black leading-normal h-full relative">
+<div id="app" class="h-full relative">
+    @yield('content')
+</div>
+<script src="{{ mix('js/app.js') }}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD7dKISLlbFBBiIyOpIOVjYqDBykD432LM&libraries=places&callback=app.initGooglePlacesAutocomplete" async defer></script>
 </body>
 </html>
