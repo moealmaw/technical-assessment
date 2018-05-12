6093aee52b33faba841ccdcbc8e51f8ed6d238bc
Mohammad Almawali <moealmaw@gmail.com>
2018-05-12 20:58:55 +0300

    date picker extension vue component

---
+++ resources/assets/js/components/DatepickerComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <flat-pickr :config="config" v-model="date" class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4" name="trip_date"></flat-pickr>
+    </div>
+</template>
+
+<script>
+    import flatPickr from 'vue-flatpickr-component';
+    import 'flatpickr/dist/flatpickr.css';
+    const today = new Date();
+    export default {
+        props: ['classname', 'defaultDate'],
+        data () {
+            return {
+                date: null,
+                minTripStartDate: null,
+                maxTripStartDate: null,
+                config: {
+                    altFormat: "Y-m-d",
+                    altInput: true,
+                    minDate: "today",
+                    defaultDate: this.date || today,
+                    dateFormat: "Y-m-d"
+                }
+            }
+        },
+        mounted() {
+            this.setSelectedDate();
+        },
+        methods: {
+            setSelectedDate: function () {
+                try{
+                    this.date = new Date(this.defaultDate).toISOString();
+                }
+                catch (err){
+                }
+            }
+        },
+        components: {
+            flatPickr
+        }
+    }
+</script>
