64fb098a31646fa7e81dc6d9d028012bb79824d6
Mohammad Almawali <moealmaw@gmail.com>
2018-05-13 08:41:13 +0300

    App title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'>
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Laravel</title>
+    <title>TravOffers</title>
 
     <!-- Fonts -->
     <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
