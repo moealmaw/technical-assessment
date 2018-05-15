6732e56a8f7575cfe69d36609171513df7dfa1cf
Mohammad Almawali <moealmaw@gmail.com>
2018-05-15 03:45:06 +0300

    Custom Exception for the API

---
+++ app/Exceptions/ApiException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+/**
+ * Class ApiException
+ *
+ * @package App\Exceptions
+ *
+ * @author Mohammad Almawali <moealmaw@gmail.com>
+ */
+class ApiException extends Exception
+{
+    //
+}
